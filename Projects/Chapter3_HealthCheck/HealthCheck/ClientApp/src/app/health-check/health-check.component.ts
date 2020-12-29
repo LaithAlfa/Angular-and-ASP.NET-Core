5cd145a62a9868c2a0ffe96009f26b7582c46039
laithalfa <laith.alfaloujeh@gmail.com>
2020-12-28 22:04:58 -0600

    added html file

--- Projects/Chapter3_HealthCheck/HealthCheck/ClientApp/src/app/health-check/health-check.component.ts
+++ Projects/Chapter3_HealthCheck/HealthCheck/ClientApp/src/app/health-check/health-check.component.ts
@@ -1,12 +1,33 @@
+//At the start of the file, we made sure to import all the Angular directives,
+//pipes, services, and Components—in one word, modules—that we need
+//throughout the whole class.
+
 import {Component, Inject } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
+
+
+
+
+
+/*In the Component's constructor, we instantiated a HttpClient service and
+a baseUrl variable using Dependency Injection (DI); the baseUrl value
+is being set by making use of the BASE_URL provider, defined in the
+/ClientApp/src/main.ts file, which we briefly introduced in Chapter
+2, Looking Around. As we can see by looking at that file's source code, it will
+resolve to our application's root URL: such a value is required by the
+HttpClient service, to build the URL that will be used to fetch the data
+from the server with an HTTP GET request to the .NET HealthChecks
+middleware that we set up earlier on (see the 'hc' string).
+*/
 
 @Component({
     selector: 'app=health-check',
     templateUrl: './health-check.component.html',
     styleUrls: ['./health-check.component.css']
   })
+
+
 
 export class HealthCheckComponent {
   public result: Result;
@@ -23,6 +44,12 @@ export class HealthCheckComponent {
     }
 }
 
+/*
+Last but not least, we defined two interfaces to deal with the JSON request
+we're expecting to receive from the HealthChecks middleware: Result
+and Check, which we designed to host the whole JSON resulting object and
+each element of the internal array, respectively.
+*/
 
 interface Result {
   checks: Check[];
