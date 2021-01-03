9266bb53fba90cc49f7d22c569355543885dfecc
laithalfa <laith.alfaloujeh@gmail.com>
2021-01-02 23:36:47 -0600

    added the excel spreed sheet

--- Projects/Chapter4_WorldCities/WorldCities/Controllers/SeedController.cs
+++ Projects/Chapter4_WorldCities/WorldCities/Controllers/SeedController.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WorldCities.Data;
+using OfficeOpenXml;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldCities.Data.Models;
+using System.Text.Json;
 
 namespace WorldCities.Controllers
 {
@@ -13,9 +18,62 @@ namespace WorldCities.Controllers
     public class SeedController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public SeedController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _env;
+        public SeedController(ApplicationDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Import()
+        {
+            var path = Path.Combine(
+                _env.ContentRootPath,
+                String.Format("Data/Source/worldCites.xlsx"));
+
+            using (var stream = new FileStream(
+                path,
+                FileMode.Open,
+                FileAccess.Read))
+                {
+                    using (var ep = new ExcelPackage(stream))
+                    {
+                        //get the first worksheet
+                        var ws = ep.Workbook.Worksheets[0];
+
+                        //initslize the record counters
+                        var nCountries = 0;
+                        var nCites = 0;
+
+                        #region Import all countries
+                        //create alist containing all the countries
+                        // already existing into the database (it will be empty on the first run)
+                        var lstCountries =_context.Countries.ToList();
+
+                        //iterates throught all rows, skipping the first one
+                        for (int nRow = 2; nRow <=ws.Dimension.End.Row; nRow++)
+                        {
+                            var row = ws.Cells[nRow, 1, nRow, ws.Dimension.End.Column];
+                            var name = row[nRow, 5].GetValue<string>();
+
+                            // Did we already created a country with the name?
+                            if (lstCountries.Where(c => c.Name == name ).Count()==0)
+                            {
+                                //create the country entity and fill it with xlsx data
+                                
+                            }
+
+
+                        }
+
+
+
+
+
+                    }
+                }
+            
         }
     }
 }
--- Projects/Chapter4_WorldCities/readme.md
+++ Projects/Chapter4_WorldCities/readme.md
@@ -1,3 +1,7 @@
 Install-Package Microsoft.EntityFrameworkCore -Version 5.0.1
+
 Install-Package Microsoft.EntityFrameworkCore.Tools -Version 5.0.1
+
 Install-Package Microsoft.EntityFrameworkCore.SqlServer -Version 5.0.1
+
+dotnet add package EPPlus --version 5.5.0
