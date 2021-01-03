39a0a716cfa3926d6d99cabae335d77eba9ae8f2
laithalfa <laith.alfaloujeh@gmail.com>
2021-01-03 00:12:44 -0600

    Added two controller

--- Projects/Chapter4_WorldCities/WorldCities/Controllers/SeedController.cs
+++ Projects/Chapter4_WorldCities/WorldCities/Controllers/SeedController.cs
@@ -15,7 +15,7 @@ namespace WorldCities.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SeedController : Controller
+    public class SeedController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
         private readonly IWebHostEnvironment _env;
@@ -44,7 +44,7 @@ namespace WorldCities.Controllers
 
                         //initslize the record counters
                         var nCountries = 0;
-                        var nCites = 0;
+                        var nCities = 0;
 
                         #region Import all countries
                         //create alist containing all the countries
@@ -61,16 +61,53 @@ namespace WorldCities.Controllers
                             if (lstCountries.Where(c => c.Name == name ).Count()==0)
                             {
                                 //create the country entity and fill it with xlsx data
+                                var country = new Country();
+                                country.Name = name;
+                                country.ISO2 = row[nRow, 6].GetValue<string>();
+                                country.ISO3 = row[nRow, 7].GetValue<string>();
+                                // save it into the Database
+                                _context.Countries.Add(country);
+                                await _context.SaveChangesAsync();
+
+                                // store the country to retrieve its Id later on 
+                                lstCountries.Add(country);
                                 
+                                //increament the counter
+                                nCountries++;
                             }
-
-
                         }
 
+                        #endregion
 
+                        #region Inport all Cities
+                        // iterates Through all rows, Skipping the first one 
+                        for(int nRow = 2; nRow <=ws.Dimension.End.Row; nRow++)
+                        {
+                            var row =ws.Cells[nRow, 1, nRow, ws.Dimension.End.Column];
+                            //create the city entity and fill it with xlsx data
+                            var city = new City();
+                            city.Name = row[nRow, 1].GetValue<string>();
+                            city.Name_ASCII = row[nRow, 2].GetValue<string>();
+                            city.Lat = row[nRow, 3].GetValue<decimal>();
+                            city.Lon = row[nRow, 4].GetValue<decimal>();
 
+                            //retrieve CountryId
+                            var countryName = row[nRow, 5].GetValue<string>();
+                            var country = lstCountries.Where(c => c.Name == countryName).FirstOrDefault();
 
+                                city.CountryId = country.Id;
+                                // save the city into the Database
+                                _context.Cities.Add(city);
+                                await _context.SaveChangesAsync();
+                                // increment the counter
+                                nCities++;
+                            }
+                            #endregion
 
+                            return new JsonResult(new {
+                                Cities = nCities,
+                                Countries = nCountries
+                            });
                     }
                 }
             
